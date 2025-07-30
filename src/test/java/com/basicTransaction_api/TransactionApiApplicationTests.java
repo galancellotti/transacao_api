cea96701cffbbea7a9d3119087c3945767f90e43
galancellotti <gabriellancellotti19@gmail.com>
2025-07-30 00:10:56 -0300

    Configuração Inicial

---
+++ src/test/java/com/basicTransaction_api/TransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicTransaction_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
